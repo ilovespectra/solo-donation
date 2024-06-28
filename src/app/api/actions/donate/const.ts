6f0f469ded1891b9e4172d41132e05dbd5aa80ce
tanny <94848716+ilovespectra@users.noreply.github.com>
2024-06-28 17:16:07 -0500

    first commit

---
+++ src/app/api/actions/donate/const.ts
@@ -0,0 +1,7 @@
+import { PublicKey } from "@solana/web3.js";
+
+export const DEFAULT_SOL_ADDRESS: PublicKey = new PublicKey(
+  "2GbS1nfoRUCGQEmuXmyaNDcoUBsUKoKVg58boiyiGuav", // mainnet wallet
+);
+
+export const DEFAULT_SOL_AMOUNT: number = 1.0;
