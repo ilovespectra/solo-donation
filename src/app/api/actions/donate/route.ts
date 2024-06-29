d5062cf825d93a2b16a06214224c68ee1f386006
tanny <94848716+ilovespectra@users.noreply.github.com>
2024-06-29 17:48:29 -0500

    change cors settings

--- src/app/api/actions/donate/route.ts
+++ src/app/api/actions/donate/route.ts
@@ -1,175 +1,172 @@
 import {
-    ActionPostResponse,
-    ACTIONS_CORS_HEADERS,
-    createPostResponse,
-    ActionGetResponse,
-    ActionPostRequest,
-  } from "@solana/actions";
-  import {
-    clusterApiUrl,
-    Connection,
-    PublicKey,
-    SystemProgram,
-    Transaction,
-    LAMPORTS_PER_SOL,
-  } from "@solana/web3.js";
-  import { DEFAULT_SOL_ADDRESS, DEFAULT_SOL_AMOUNT } from "./const";
-  
-  export const GET = async (req: Request) => {
-    try {
-      const requestUrl = new URL(req.url);
-      const { toPubkey } = validatedQueryParams(requestUrl);
-  
-      const baseHref = new URL(
-        `/api/actions/donate?to=${toPubkey.toBase58()}`,
-        requestUrl.origin,
-      ).toString();
-  
-      const payload: ActionGetResponse = {
-        title: "donate sol to solo",
-        icon: new URL("/donate.png", requestUrl.origin).toString(),
-        description: "donate sol to solo",
-        label: "transfer", 
-        links: {
-          actions: [
-            {
-              label: "send 1 sol", 
-              href: `${baseHref}&amount=${"1"}`,
-            },
-            {
-              label: "send 5 sol", 
-              href: `${baseHref}&amount=${"5"}`,
-            },
-            {
-              label: "send 10 sol", 
-              href: `${baseHref}&amount=${"10"}`,
-            },
-            {
-              label: "send sol", 
-              href: `${baseHref}&amount={amount}`, 
-              parameters: [
-                {
-                  name: "amount", 
-                  label: "enter the amount of sol to send", 
-                  required: true,
-                },
-              ],
-            },
-          ],
-        },
-      };
-  
-      return new Response(JSON.stringify(payload), {
-        headers: ACTIONS_CORS_HEADERS,
-      });
-    } catch (err) {
-      console.error(err);
-      let message = "an unknown error occurred";
-      if (typeof err == "string") message = err;
-      return new Response(message, {
-        status: 400,
-        headers: ACTIONS_CORS_HEADERS,
-      });
-    }
-  };
-  
-  // DO NOT FORGET TO INCLUDE THE `OPTIONS` HTTP METHOD
-  // THIS WILL ENSURE CORS WORKS FOR BLINKS
-  export const OPTIONS = GET;
-  
-  export const POST = async (req: Request) => {
-    try {
-      const requestUrl = new URL(req.url);
-      const { amount, toPubkey } = validatedQueryParams(requestUrl);
-  
-      const body: ActionPostRequest = await req.json();
-  
-      // validate the client provided input
-      let account: PublicKey;
-      try {
-        account = new PublicKey(body.account);
-      } catch (err) {
-        return new Response('invalid "account" provided', {
-          status: 400,
-          headers: ACTIONS_CORS_HEADERS,
-        });
-      }
-  
-      const connection = new Connection(
-        process.env.NEXT_PUBLIC_SOLANA_RPC! || clusterApiUrl("mainnet-beta"),
-      );
-  
-      // ensure the receiving account will be rent exempt
-      const minimumBalance = await connection.getMinimumBalanceForRentExemption(
-        0, // note: simple accounts that just store native SOL have `0` bytes of data
-      );
-      if (amount * LAMPORTS_PER_SOL < minimumBalance) {
-        throw `account may not be rent exempt: ${toPubkey.toBase58()}`;
-      }
-  
-      const transaction = new Transaction();
-      transaction.feePayer = account;
-  
-      transaction.add(
-        SystemProgram.transfer({
-          fromPubkey: account,
-          toPubkey: toPubkey,
-          lamports: amount * LAMPORTS_PER_SOL,
-        }),
-      );
-  
-      transaction.recentBlockhash = (
-        await connection.getLatestBlockhash()
-      ).blockhash;
-  
-      const payload: ActionPostResponse = await createPostResponse({
-        fields: {
-          transaction,
-          message: `send ${amount} sol to ${toPubkey.toBase58()}`,
-        },
-        // note: no additional signers are needed
-        // signers: [],
-      });
-  
-      return new Response(JSON.stringify(payload), {
-        headers: ACTIONS_CORS_HEADERS,
-      });
-    } catch (err) {
-      console.error(err);
-      let message = "an unknown error occurred";
-      if (typeof err == "string") message = err;
-      return new Response(message, {
-        status: 400,
-        headers: ACTIONS_CORS_HEADERS,
-      });
-    }
-  };
-  
-  function validatedQueryParams(requestUrl: URL) {
-    let toPubkey: PublicKey = DEFAULT_SOL_ADDRESS;
-    let amount: number = DEFAULT_SOL_AMOUNT;
-  
-    try {
-      if (requestUrl.searchParams.get("to")) {
-        toPubkey = new PublicKey(requestUrl.searchParams.get("to")!);
-      }
-    } catch (err) {
-      throw "invalid input query parameter: to";
-    }
-  
-    try {
-      if (requestUrl.searchParams.get("amount")) {
-        amount = parseFloat(requestUrl.searchParams.get("amount")!);
-      }
-  
-      if (amount <= 0) throw "amount is too small";
-    } catch (err) {
-      throw "invalid input query parameter: amount";
-    }
-  
-    return {
-      amount,
-      toPubkey,
+  ActionPostResponse,
+  ACTIONS_CORS_HEADERS,
+  createPostResponse,
+  ActionGetResponse,
+  ActionPostRequest,
+} from "@solana/actions";
+import {
+  clusterApiUrl,
+  Connection,
+  PublicKey,
+  SystemProgram,
+  Transaction,
+  LAMPORTS_PER_SOL,
+} from "@solana/web3.js";
+import { DEFAULT_SOL_ADDRESS, DEFAULT_SOL_AMOUNT } from "./const";
+
+export const GET = async (req: Request) => {
+  try {
+    const requestUrl = new URL(req.url);
+    const { toPubkey } = validatedQueryParams(requestUrl);
+
+    const baseHref = new URL(
+      `/api/actions/donate?to=${toPubkey.toBase58()}`,
+      requestUrl.origin
+    ).toString();
+
+    const payload: ActionGetResponse = {
+      title: "donate sol to solo",
+      icon: new URL("/donate.png", requestUrl.origin).toString(),
+      description: "donate sol to solo",
+      label: "transfer",
+      links: {
+        actions: [
+          {
+            label: "send 1 sol",
+            href: `${baseHref}&amount=1`,
+          },
+          {
+            label: "send 5 sol",
+            href: `${baseHref}&amount=5`,
+          },
+          {
+            label: "send 10 sol",
+            href: `${baseHref}&amount=10`,
+          },
+          {
+            label: "send sol",
+            href: `${baseHref}&amount={amount}`,
+            parameters: [
+              {
+                name: "amount",
+                label: "enter the amount of sol to send",
+                required: true,
+              },
+            ],
+          },
+        ],
+      },
     };
+
+    return new Response(JSON.stringify(payload), {
+      headers: ACTIONS_CORS_HEADERS,
+    });
+  } catch (err) {
+    console.error(err);
+    let message = "an unknown error occurred";
+    if (typeof err == "string") message = err;
+    return new Response(message, {
+      status: 400,
+      headers: ACTIONS_CORS_HEADERS,
+    });
   }
-  
+};
+
+// DO NOT FORGET TO INCLUDE THE `OPTIONS` HTTP METHOD
+// THIS WILL ENSURE CORS WORKS FOR BLINKS
+export const OPTIONS = GET;
+
+export const POST = async (req: Request) => {
+  try {
+    const requestUrl = new URL(req.url);
+    const { amount, toPubkey } = validatedQueryParams(requestUrl);
+
+    const body: ActionPostRequest = await req.json();
+
+    // validate the client provided input
+    let account: PublicKey;
+    try {
+      account = new PublicKey(body.account);
+    } catch (err) {
+      return new Response('invalid "account" provided', {
+        status: 400,
+        headers: ACTIONS_CORS_HEADERS,
+      });
+    }
+
+    const connection = new Connection(
+      process.env.NEXT_PUBLIC_SOLANA_RPC! || clusterApiUrl("mainnet-beta")
+    );
+
+    // ensure the receiving account will be rent exempt
+    const minimumBalance = await connection.getMinimumBalanceForRentExemption(
+      0 // note: simple accounts that just store native SOL have `0` bytes of data
+    );
+    if (amount * LAMPORTS_PER_SOL < minimumBalance) {
+      throw `account may not be rent exempt: ${toPubkey.toBase58()}`;
+    }
+
+    const transaction = new Transaction();
+    transaction.feePayer = account;
+
+    transaction.add(
+      SystemProgram.transfer({
+        fromPubkey: account,
+        toPubkey: toPubkey,
+        lamports: amount * LAMPORTS_PER_SOL,
+      })
+    );
+
+    transaction.recentBlockhash = (
+      await connection.getLatestBlockhash()
+    ).blockhash;
+
+    const payload: ActionPostResponse = await createPostResponse({
+      fields: {
+        transaction,
+        message: `send ${amount} sol to ${toPubkey.toBase58()}`,
+      },
+    });
+
+    return new Response(JSON.stringify(payload), {
+      headers: ACTIONS_CORS_HEADERS,
+    });
+  } catch (err) {
+    console.error(err);
+    let message = "an unknown error occurred";
+    if (typeof err == "string") message = err;
+    return new Response(message, {
+      status: 400,
+      headers: ACTIONS_CORS_HEADERS,
+    });
+  }
+};
+
+function validatedQueryParams(requestUrl: URL) {
+  let toPubkey: PublicKey = DEFAULT_SOL_ADDRESS;
+  let amount: number = DEFAULT_SOL_AMOUNT;
+
+  try {
+    if (requestUrl.searchParams.get("to")) {
+      toPubkey = new PublicKey(requestUrl.searchParams.get("to")!);
+    }
+  } catch (err) {
+    throw "invalid input query parameter: to";
+  }
+
+  try {
+    if (requestUrl.searchParams.get("amount")) {
+      amount = parseFloat(requestUrl.searchParams.get("amount")!);
+    }
+
+    if (amount <= 0) throw "amount is too small";
+  } catch (err) {
+    throw "invalid input query parameter: amount";
+  }
+
+  return {
+    amount,
+    toPubkey,
+  };
+}
