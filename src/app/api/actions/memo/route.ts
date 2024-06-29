5c730a90179679fa982193fc37f203dd396cd45c
tanny <94848716+ilovespectra@users.noreply.github.com>
2024-06-29 17:23:23 -0500

    revert

--- src/app/api/actions/memo/route.ts
+++ src/app/api/actions/memo/route.ts
@@ -1,90 +1,67 @@
-import {
-    ActionPostResponse,
-    ACTIONS_CORS_HEADERS,
-    createPostResponse,
-    MEMO_PROGRAM_ID,
-    ActionGetResponse,
-    ActionPostRequest,
-  } from "@solana/actions";
-  import {
-    clusterApiUrl,
-    ComputeBudgetProgram,
-    Connection,
-    PublicKey,
-    Transaction,
-    TransactionInstruction,
-  } from "@solana/web3.js";
-  import { NextRequest, NextResponse } from 'next/server';
-  
-  export const GET = async (req: NextRequest) => {
-    const payload: ActionGetResponse = {
-      title: "Actions Example - Simple On-chain Memo",
-      icon: new URL("/solana_devs.jpg", req.nextUrl.origin).toString(),
-      description: "Send a message on-chain using a Memo",
-      label: "Send Memo",
+import { ACTIONS_CORS_HEADERS, ActionGetResponse, ActionPostRequest, ActionPostResponse, MEMO_PROGRAM_ID, createPostResponse } from "@solana/actions";
+import { Connection } from "@solana/web3.js";
+import { clusterApiUrl } from "@solana/web3.js";
+import { ComputeBudgetProgram, PublicKey, Transaction, TransactionInstruction } from "@solana/web3.js";
+
+export const GET = (req: Request) => {
+    const payload : ActionGetResponse = {
+        icon: new URL("/solo.png", new URL(req.url).origin).toString(),
+        label: "solo memo",
+        description: "this is a memo action for solo",
+        title: "memo action",
     };
-  
-    return NextResponse.json(payload);
-  };
-  
-  export const OPTIONS = async (req: NextRequest) => {
-    // OPTIONS method is automatically handled by Next.js for CORS, no need to manually set headers
-    return NextResponse.json({});
-  };
-  
-  export const POST = async (req: NextRequest) => {
+
+    return Response.json(payload, {
+        headers: ACTIONS_CORS_HEADERS
+    });
+};
+
+export const OPTIONS = GET;
+
+export const POST = async (req: Request) => {
+
     try {
-      const body: ActionPostRequest = await req.json();
-  
-      let account: PublicKey;
-      try {
-        account = new PublicKey(body.account);
-      } catch (err) {
-        return NextResponse.json({ error: 'Invalid "account" provided' }, {
-          status: 400,
-        });
-      }
-  
-      const connection = new Connection(
-        process.env.SOLANA_RPC! || clusterApiUrl("devnet"),
-      );
-  
-      const transaction = new Transaction().add(
-        // note: `createPostResponse` requires at least 1 non-memo instruction
-        ComputeBudgetProgram.setComputeUnitPrice({
-          microLamports: 1000,
-        }),
+
+        const body: ActionPostRequest = await req.json();
+
+        let account: PublicKey;
+        try {
+            account = new PublicKey(body.account);
+        } catch (err) {
+            return new Response('invalid "account" provided', {
+                status: 400,
+                headers: ACTIONS_CORS_HEADERS,
+            })
+        }
+
+        const transaction = new Transaction();
+
+        transaction.add(
+            ComputeBudgetProgram.setComputeUnitPrice({
+                microLamports: 1000,
+            }),
+        
         new TransactionInstruction({
-          programId: new PublicKey(MEMO_PROGRAM_ID),
-          data: Buffer.from("this is a simple memo message2", "utf8"),
-          keys: [],
+            programId: new PublicKey(MEMO_PROGRAM_ID),
+            data: Buffer.from("this is a simple message", "utf8"),
+            keys: [],
         }),
       );
-  
-      // set the end user as the fee payer
+
       transaction.feePayer = account;
-  
-      transaction.recentBlockhash = (
-        await connection.getLatestBlockhash()
-      ).blockhash;
-  
-      const payload: ActionPostResponse = await createPostResponse({
+
+      const payload :ActionPostResponse = await createPostResponse({
         fields: {
-          transaction,
-          message: "Post this memo on-chain",
+            transaction
         },
-        // no additional signers are required for this transaction
-        // signers: [],
-      });
-  
-      return NextResponse.json(payload);
+        // signers: []
+      })
+      const connection = new Connection(clusterApiUrl("devnet"));
+      transaction.recentBlockhash = (await connection.getLatestBlockhash()).blockhash;
+
+      return Response.json(payload, { headers: ACTIONS_CORS_HEADERS });
     } catch (err) {
-      console.log(err);
-      let message = "An unknown error occurred";
-      if (typeof err == "string") message = err;
-      return NextResponse.json({ error: message }, {
-        status: 400,
-      });
+        return Response.json("an unknown error occcured", { status: 404 })
     }
-  };
-  
+
+}
