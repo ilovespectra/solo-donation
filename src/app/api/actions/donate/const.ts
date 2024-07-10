916b265c34f73f630f4736c28917414983c4e1c5
tanny <94848716+ilovespectra@users.noreply.github.com>
2024-07-10 09:29:42 -0400

    so1o pubkey

--- src/app/api/actions/donate/const.ts
+++ src/app/api/actions/donate/const.ts
@@ -1,7 +1,7 @@
 import { PublicKey } from "@solana/web3.js";
 
 export const DEFAULT_SOL_ADDRESS: PublicKey = new PublicKey(
-  "2GbS1nfoRUCGQEmuXmyaNDcoUBsUKoKVg58boiyiGuav", // soloexplorer.sol mainnet wallet
+  "so1o2fdDB4mcUn4pQkhS9E47X9LEg9beDYEbx6sYnX8", // soloexplorer.sol mainnet wallet
 );
 
 export const DEFAULT_SOL_AMOUNT: number = 1.0;
